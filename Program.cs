6f8354ab693dd952a267f6e810b4e296a02fa517
fitosjozsef0309 <fitos.jozsef.2024d@telekimezotur.hu>
2026-09-17 12:38:50 +0200

    Elso

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Kérem a nevet: ");
+string Nev = Console.ReadLine();
+
+Console.WriteLine("Kérem a megtett távot (km): ");
+double Tav = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Északai volt: ");
+var Eszak = Convert.ToBoolean(Console.ReadLine());
+
+double Alapdij = 1100;
+
+double kilometerdij = 400;
+
+double Eszakaidij = 1.20;
 
