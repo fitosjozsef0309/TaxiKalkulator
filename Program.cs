4a0f29a27579dae5fcffc3a98339da7573ca776b
fitosjozsef0309 <fitos.jozsef.2024d@telekimezotur.hu>
2026-09-17 12:48:53 +0200

    3.fel

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@ string Nev = Console.ReadLine();
 Console.WriteLine("Kérem a megtett távot (km): ");
 double Tav = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Északai volt: ");
-var Eszak = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("Északai volt(igen=1/nem=0): ");
+int Eszaka = Convert.ToInt32(Console.ReadLine());
 
 double Alapdij = 1100;
 
@@ -13,3 +13,19 @@ double kilometerdij = 400;
 
 double Eszakaidij = 1.20;
 
+var eszakai = false;
+
+double Fizetendo = Alapdij + (Tav * kilometerdij) ;
+
+if (Eszaka == 1)
+{
+    eszakai = true;
+}
+
+if (eszakai==true)
+{
+    Fizetendo *= Eszakaidij;
+};
+
+
+Console.WriteLine($"A fizetendő összeg: {Fizetendo} Ft");
